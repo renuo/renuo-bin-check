d3aaf57993ee5c57bcbf0a151b037895e0158646
Zora Fuchs <zora.fuchs@renuo.ch>
2016-04-26 11:08:35 +0200

    add one simple test

---
+++ app/test_class.rb
@@ -0,0 +1,5 @@
+class TestClass
+  def true_method
+    true
+  end
+end
---
+++ spec/test_class_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require './app/test_class'
+
+RSpec.describe 'simple test' do
+  it 'should return true' do
+    test_class = TestClass.new
+    expect(test_class.true_method).to be_truthy
+  end
+end
