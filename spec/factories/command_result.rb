868d244e49fb54219fedbf13dccfd0094776c1c1
Zora Fuchs <zora.fuchs@renuo.ch>
2016-05-04 14:40:22 +0200

    implement run method of ServantThread that runs the command given in script_config 	- create test-scripts for testing use 	- adapt factory to match scripts 	- implement method

--- spec/factories/command_result.rb
+++ spec/factories/command_result.rb
@@ -1,9 +1,10 @@
 require './lib/renuo/bin-check/command_result'
 FactoryGirl.define do
   factory :result, class: RenuoBinCheck::CommandResult do
-    output 'No errors, yeyy!!'
-    error_output 'Oh no an error :('
-    exit_code '0'
+    output "I have been running\nNow I'm tired\n"
+    error_output "I couldn't run :'(\nplease motivate me to\n"
+    exit_code 0
+    initialize_with { new(output, error_output, exit_code) }
 
     factory :faild_result do
       return_value '1'
--- spec/renuo/bin-check/servant_thread_spec.rb
+++ spec/renuo/bin-check/servant_thread_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 require './lib/renuo/bin-check/servant_thread'
 
 RSpec.describe RenuoBinCheck::ServantThread do
-  let(:script) { build :script }
-  it 'initializes the instance variable script with the given ScriptConfig' do
-    servant = RenuoBinCheck::ServantThread.new(script)
-    expect(servant.script).to eq(script)
+  context 'initializing' do
+    let(:script) { build :script }
+    it 'initializes the instance variable script with the given ScriptConfig' do
+      servant = RenuoBinCheck::ServantThread.new(script)
+      expect(servant.script).to eq(script)
+    end
+  end
+
+  context 'running successfully' do
+    let(:script) { build :script, script_command: './spec/spec-files/test_script_exit0' }
+    let(:servant) { RenuoBinCheck::ServantThread.new(script) }
+    let(:result_attributes) { attributes_for :result }
+
+    it 'starts the command defined in ScriptConfig and returns a Result' do
+      expect(servant.run).to have_attributes(result_attributes)
+    end
   end
 end
