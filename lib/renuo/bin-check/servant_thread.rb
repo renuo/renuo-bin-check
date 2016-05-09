9a48da0a77612cb0e5a92d01c2f433cd3c335760
Zora Fuchs <zora.fuchs@renuo.ch>
2016-05-09 15:42:20 +0200

    make servant use caching

--- lib/renuo/bin-check/servant_thread.rb
+++ lib/renuo/bin-check/servant_thread.rb
@@ -4,12 +4,22 @@ module RenuoBinCheck
     attr_accessor :script
     def initialize(script_config)
       @script = script_config
+      @cacher = Cacher.new(@script.script_command, @script.script_files)
     end
 
     def run
-      Open3.popen3(@script.script_command) do |_stdin, stdout, stderr, wait_thr|
-        CommandResult.new(stdout.read, stderr.read, wait_thr.value.exitstatus)
+      if cache.class == String
+        hash = @result
+        Open3.popen3(@script.script_command) do |_stdin, stdout, stderr, wait_thr|
+          @result = CommandResult.new(stdout.read, stderr.read, wait_thr.value.exitstatus)
+        end
+        @cacher.cache(hash, @result)
       end
+      @result
+    end
+
+    def cache
+      @result = @cacher.result
     end
   end
 end
--- spec/renuo/bin-check/servant_thread_spec.rb
+++ spec/renuo/bin-check/servant_thread_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require './lib/renuo/bin-check/servant_thread'
 
 RSpec.describe RenuoBinCheck::ServantThread do
+  let(:cacher) { build :not_found_cacher }
+  let(:result_attributes) { attributes_for :result }
   context 'initializing' do
     let(:script) { build :script }
     it 'initializes the instance variable script with the given ScriptConfig' do
@@ -13,9 +15,20 @@ RSpec.describe RenuoBinCheck::ServantThread do
   context 'running successfully' do
     let(:script) { build :script, script_command: './spec/spec-files/test_script_exit0' }
     let(:servant) { RenuoBinCheck::ServantThread.new(script) }
-    let(:result_attributes) { attributes_for :result }
 
     it 'starts the command defined in ScriptConfig and returns a Result' do
+      expect(servant.run).to have_attributes(result_attributes)
+    end
+  end
+
+  context 'finding cache' do
+    let(:script) do
+      build :script, script_command: 'script_name',
+                     script_files: ['./spec/spec-files/file1', './spec/spec-files/file2']
+    end
+    let(:servant) { RenuoBinCheck::ServantThread.new(script) }
+
+    it 'gets result from cache' do
       expect(servant.run).to have_attributes(result_attributes)
     end
   end
