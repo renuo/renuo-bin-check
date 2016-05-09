fcdbdc636d728b9cb49d5e6d3723ed4d4cb7b875
Zora Fuchs <zora.fuchs@renuo.ch>
2016-05-09 11:47:29 +0200

    implement method to cache a result for the given path

--- lib/renuo/bin-check/cacher.rb
+++ lib/renuo/bin-check/cacher.rb
@@ -1,11 +1,20 @@
 require 'digest'
+require 'fileutils'
 
 module RenuoBinCheck
   class Cacher
     def result(paths, command)
       files = paths.map { |path| Dir[path] }.flatten.sort
-      hash = Digest::MD5.hexdigest files.map { |file| Digest::MD5.file(file).hexdigest if File.file? file }.to_s
-      read(hash, command) if File.exist? "tmp/bin-check/#{command}/#{hash}"
+      hash = hash_files(files)
+      File.exist?("tmp/bin-check/#{command}/#{hash}") ? read(hash, command) : hash
+    end
+
+    #:reek:UtilityFunction
+    def cache(result, command, hash)
+      FileUtils.mkdir_p "tmp/bin-check/#{command}/#{hash}"
+      File.write "tmp/bin-check/#{command}/#{hash}/output", result.output
+      File.write "tmp/bin-check/#{command}/#{hash}/error_output", result.error_output
+      File.write "tmp/bin-check/#{command}/#{hash}/exit_code", result.exit_code
     end
 
     private
@@ -16,6 +25,11 @@ module RenuoBinCheck
       error_output = File.read("tmp/bin-check/#{command}/#{hash}/error_output")
       exit_code = File.read("tmp/bin-check/#{command}/#{hash}/exit_code").to_i
       CommandResult.new(output, error_output, exit_code)
+    end
+
+    #:reek:UtilityFunction
+    def hash_files(files)
+      Digest::MD5.hexdigest files.map { |file| Digest::MD5.file(file).hexdigest if File.file? file }.to_s
     end
   end
 end
--- spec/renuo/bin-check/cacher_spec.rb
+++ spec/renuo/bin-check/cacher_spec.rb
@@ -3,18 +3,30 @@ require './lib/renuo/bin-check/cacher'
 
 RSpec.describe RenuoBinCheck::Cacher do
   let(:cacher) { RenuoBinCheck::Cacher.new }
-  let(:result) { attributes_for(:result) }
+  let(:result_attributes) { attributes_for(:result) }
+  let(:result) { build(:result) }
 
   context 'results are cashed' do
     it 'returns result' do
       expect(cacher.result(['./spec/spec-files/file1', './spec/spec-files/file2'],
-                           'script-name')).to have_attributes(result)
+                           'script-name')).to have_attributes(result_attributes)
     end
   end
 
   context 'cashes do not exist' do
-    it 'returns nil' do
-      expect(cacher.result(['./spec/spec-files/file2', './spec/spec-files/file1'], 'script2-name')).to be_falsey
+    it 'returns new hash' do
+      expect(cacher.result(['./spec/spec-files/file2', './spec/spec-files/file1'], 'script2-name'))
+        .to eq('f75c3cee2826ea881cb41b70b2d333b1')
     end
+  end
+
+  it 'saves result to files' do
+    cacher.cache(result, 'script-name', 'f75c5cee2826ea881cb81b70b2d333b7')
+    expect(File.read('./tmp/bin-check/script-name/f75c5cee2826ea881cb81b70b2d333b7/error_output'))
+      .to eq(result.error_output)
+    expect(File.read('./tmp/bin-check/script-name/f75c5cee2826ea881cb81b70b2d333b7/error_output'))
+      .to eq(result.error_output)
+    expect(File.read('./tmp/bin-check/script-name/f75c5cee2826ea881cb81b70b2d333b7/error_output'))
+      .to eq(result.error_output)
   end
 end
