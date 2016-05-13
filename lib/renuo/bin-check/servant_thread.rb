112af17be76191022ed9d765009ba4aeb5b1efaf
Zora Fuchs <zora.fuchs@renuo.ch>
2016-05-13 09:43:08 +0200

    rename script variable of ServantThread to script_config

--- lib/renuo/bin-check/servant_thread.rb
+++ lib/renuo/bin-check/servant_thread.rb
@@ -1,15 +1,15 @@
 require 'open3'
 module RenuoBinCheck
   class ServantThread
-    attr_reader :script
+    attr_reader :script_config
     def initialize(script_config)
-      @script = script_config
-      script_files = @script.script_files
-      @cacher = Cacher.new(@script.script_name, script_files) if script_files
+      @script_config = script_config
+      script_files = @script_config.script_files
+      @cacher = Cacher.new(@script_config.script_name, script_files) if script_files
     end
 
     def run
-      @script.script_files ? run_with_cache : run_command
+      @script_config.script_files ? run_with_cache : run_command
     end
 
     private
@@ -20,7 +20,7 @@ module RenuoBinCheck
     end
 
     def run_command
-      Open3.popen3(@script.script_command) do |_stdin, stdout, stderr, wait_thr|
+      Open3.popen3(@script_config.script_command) do |_stdin, stdout, stderr, wait_thr|
         @result = Result.new(stdout.read, stderr.read, wait_thr.value.exitstatus)
       end
     end
--- spec/renuo/bin-check/servant_thread_spec.rb
+++ spec/renuo/bin-check/servant_thread_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe RenuoBinCheck::ServantThread do
     let(:script) { build :script }
     it 'initializes the instance variable script with the given ScriptConfig' do
       servant = RenuoBinCheck::ServantThread.new(script)
-      expect(servant.script).to eq(script)
+      expect(servant.script_config).to eq(script)
     end
 
     it 'initializes cacher with script_name' do
