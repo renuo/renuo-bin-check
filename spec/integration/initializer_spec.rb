b84e3160177d0f1c6d0859dfe1bb2c1650cf2bdf
Zora Fuchs <zora.fuchs@renuo.ch>
2016-05-13 13:35:19 +0200

    add integration tests as they are described in the documentation

--- spec/integration/initializer_spec.rb
+++ spec/integration/initializer_spec.rb
@@ -4,14 +4,10 @@ require './lib/renuo/bin-check/initializer'
 RSpec.describe RenuoBinCheck::Initializer do
   let(:bin_check) { RenuoBinCheck::Initializer.new }
 
-  context 'passing script' do
-    after(:each) { FileUtils.remove_dir('./tmp/bin-check/exit0') }
-
-    it 'runns the whole application as expected' do
+  context 'without caching' do
+    it 'returns exit-code 0 and expected output' do
       bin_check.check do |config|
         config.command './spec/spec-files/test_script_exit0'
-        config.name 'exit0'
-        config.files %w(file1 file2)
       end
       expect do
         begin
@@ -21,16 +17,23 @@ RSpec.describe RenuoBinCheck::Initializer do
         end
       end.to output("I passed\nThis is the second line\n").to_stdout
     end
-  end
 
-  context 'failing script' do
-    after(:each) { FileUtils.remove_dir('./tmp/bin-check/exit1') }
+    it 'returns exit-code 0 and expected output' do
+      bin_check.check do |config|
+        config.command 'echo hello'
+      end
+      expect do
+        begin
+          bin_check.run
+        rescue SystemExit => se
+          expect(se.status).to eq(0)
+        end
+      end.to output.to_stdout
+    end
 
-    it 'runns the whole application as expected' do
+    it 'returns exit-code 1 and expected error-output' do
       bin_check.check do |config|
         config.command './spec/spec-files/test_script_exit1'
-        config.files %w(file1 file2)
-        config.name 'exit1'
       end
       expect do
         begin
@@ -40,6 +43,22 @@ RSpec.describe RenuoBinCheck::Initializer do
         end
       end.to output("I failed\nThis is the second line\n").to_stderr
     end
+
+    ## This test is temporary commented out, as it is not passing. Investigations are in process.
+    # it 'runns scripts parallel' do
+    #   start_time = Time.now
+    #   bin_check.check { |config| config.command './spec/spec-files/test_script_sleep1' }
+    #   bin_check.check { |config| config.command './spec/spec-files/test_script_sleep2' }
+    #   # expect do
+    #   begin
+    #     bin_check.run
+    #   rescue SystemExit => se
+    #     expect(se.status).to eq(0)
+    #   end
+    #   # end.to output("I passed\nThis is the second line\n").to_stdout
+    #   end_time = Time.now
+    #   expect(end_time - start_time).to eq(2)
+    # end
   end
 
   context 'cached script' do
@@ -54,7 +73,7 @@ RSpec.describe RenuoBinCheck::Initializer do
 
     after(:each) { FileUtils.remove_dir('./tmp/bin-check/exit0') }
 
-    it 'runns the whole application as expected' do
+    it 'returns cached output and exit-code' do
       bin_check.check do |config|
         config.command './spec/spec-files/test_script_exit0'
         config.files %w(./spec/spec-files/file1 ./spec/spec-files/file2)
@@ -70,21 +89,13 @@ RSpec.describe RenuoBinCheck::Initializer do
     end
   end
 
-  context 'without defining files' do
-    before(:each) do
-      FileUtils.mkdir_p 'tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019'
-      File.write 'tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/output',
-                 "I'm cached\npassed\n"
-      File.write 'tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/error_output',
-                 "I'm cached\npassed\n"
-      File.write 'tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/exit_code', 0
-    end
-
+  context 'saving to exit0 folder' do
     after(:each) { FileUtils.remove_dir('./tmp/bin-check/exit0') }
 
-    it 'runns the whole application as expected' do
+    it 'saves output and exit-code to files in folder named by given name' do
       bin_check.check do |config|
         config.command './spec/spec-files/test_script_exit0'
+        config.files %w(./spec/spec-files/file1 ./spec/spec-files/file2)
         config.name 'exit0'
       end
       expect do
@@ -94,6 +105,38 @@ RSpec.describe RenuoBinCheck::Initializer do
           expect(se.status).to eq(0)
         end
       end.to output("I passed\nThis is the second line\n").to_stdout
+
+      expect(File.read('./tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/error_output'))
+        .to eq("I failed\nThis is the second line\n")
+      expect(File.read('./tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/output'))
+        .to eq("I passed\nThis is the second line\n")
+      expect(File.read('./tmp/bin-check/exit0/df57ab93c06ded11a01f2de950307019/exit_code').to_i).to eq(0)
+    end
+  end
+
+  context 'saving to 65a98809d7447e9857b9acf1fbc89dcc folder' do
+    after(:each) { FileUtils.remove_dir('./tmp/bin-check/65a98809d7447e9857b9acf1fbc89dcc') }
+
+    it 'saves output and exit-code to files in folder named by hashed command' do
+      bin_check.check do |config|
+        config.command './spec/spec-files/test_script_exit0'
+        config.files %w(./spec/spec-files/file1 ./spec/spec-files/file2)
+      end
+      expect do
+        begin
+          bin_check.run
+        rescue SystemExit => se
+          expect(se.status).to eq(0)
+        end
+      end.to output("I passed\nThis is the second line\n").to_stdout
+
+      expect(File.read('./tmp/bin-check/65a98809d7447e9857b9acf1fbc89dcc/'\
+                        'df57ab93c06ded11a01f2de950307019/error_output'))
+        .to eq("I failed\nThis is the second line\n")
+      expect(File.read('./tmp/bin-check/65a98809d7447e9857b9acf1fbc89dcc/df57ab93c06ded11a01f2de950307019/output'))
+        .to eq("I passed\nThis is the second line\n")
+      expect(File.read('./tmp/bin-check/65a98809d7447e9857b9acf1fbc89dcc/df57ab93c06ded11a01f2de950307019/exit_code')
+               .to_i).to eq(0)
     end
   end
 end
