3cb56e65689fd71eaadfd8ca5d5c1c4fd5b69592
Josua Schmid <josua.schmid@renuo.ch>
2016-06-15 16:32:24 +0200

    remove first test file

--- spec/test_class_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require './lib/renuo/bin-check/app/test_class'
-
-RSpec.describe 'simple test' do
-  it 'should return true' do
-    test_class = TestClass.new
-    expect(test_class.true_method).to be_truthy
-  end
-end
