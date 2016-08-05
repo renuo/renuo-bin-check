e20ba0964ffadc3bf4f5ee0e5e494fcf892dec1e
Zora Fuchs <zora.fuchs@renuo.ch>
2016-08-05 16:43:56 +0200

    really, really, really disgusting solution but it works...

--- lib/renuo_bin_check/bin_check.rb
+++ lib/renuo_bin_check/bin_check.rb
@@ -1,8 +1,14 @@
 class BinCheck
   def self.method_missing(name, *_params, &configs)
     if block_given?
-      @current_name = name
-      @new_script_hash[@current_name] = {}
+      if caller[2][/`.*'/][1..-2] == "run"
+        @new_current_name = nil
+        @current_name = name
+        @new_script_hash[@current_name] = {}
+      else
+        @new_current_name = name
+        @new_script_hash[@current_name][@new_current_name] = {}
+      end
       instance_eval(&configs)
     else
       super
@@ -18,6 +24,39 @@ class BinCheck
   def self.run(&check)
     @new_script_hash = {}
     instance_eval(&check)
+    hash_safe = {}
+    @new_script_hash.each do |key, value|
+      should_be_deleted = false
+      shared_values = {}
+      temporary_hash_safe = {}
+      if value.class == Hash
+        value.each do |blubb, blibb|
+          if blibb.class != Hash
+            shared_values[blubb] = blibb
+          else
+            shared_values.each do |bubbel, bibbel|
+              value.delete(bubbel) if value[bubbel]
+              temporary_hash_safe[blubb] = blibb
+              should_be_deleted = true
+            end
+          end
+        end
+      end
+      if should_be_deleted
+        @new_script_hash.delete(key)
+        temporary_hash_safe.each do |blubb, blibb|
+          shared_values.each do |bubbel, bibbel|
+            blibb[bubbel] = bibbel
+          end
+        end
+      end
+      temporary_hash_safe.each do |blubb, blibb|
+        hash_safe[blubb] = blibb
+      end
+    end
+    hash_safe.each do |key, value|
+      @new_script_hash[key] = value
+    end
     initialize_checks
   end
 
@@ -36,23 +75,43 @@ class BinCheck
   end
 
   def self.files(files)
-    @new_script_hash[@current_name][:files] = files
+    if @new_current_name
+      @new_script_hash[@current_name][@new_current_name][:files] = files
+    else
+      @new_script_hash[@current_name][:files] = files
+    end
   end
 
   def self.command(command)
-    @new_script_hash[@current_name][:command] = command
+    if @new_current_name
+      @new_script_hash[@current_name][@new_current_name][:command] = command
+    else
+      @new_script_hash[@current_name][:command] = command
+    end
   end
 
   def self.reversed_exit(reversed_exit)
-    @new_script_hash[@current_name][:reversed_exit] = reversed_exit
+    if @new_current_name
+      @new_script_hash[@current_name][@new_current_name][:reversed_exit] = reversed_exit
+    else
+      @new_script_hash[@current_name][:reversed_exit] = reversed_exit
+    end
   end
 
   def self.error_message(error_message)
-    @new_script_hash[@current_name][:error_message] = error_message
+    if @new_current_name
+      @new_script_hash[@current_name][@new_current_name][:error_message] = error_message
+    else
+      @new_script_hash[@current_name][:error_message] = error_message
+    end
   end
 
   def self.success_message(success_message)
-    @new_script_hash[@current_name][:success_message] = success_message
+    if @new_current_name
+      @new_script_hash[@current_name][@new_current_name][:success_message] = success_message
+    else
+      @new_script_hash[@current_name][:success_message] = success_message
+    end
   end
 
   class << self
--- spec/renuo/bin-check/bin_check_spec.rb
+++ spec/renuo/bin-check/bin_check_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe BinCheck do
              t0d0_finder: { files: 'lib/**/*.*', command: 'blubb-blubb', reversed_exit: true })
   end
 
+  it 'understands common configs' do
+    expect(BinCheck).to receive(:initialize_checks).and_return(true)
+
+    BinCheck.run do
+      ruby_files do
+        files 'lib/**/*.rb'
+        rubocop do
+          command 'rubocop'
+          reversed_exit false
+        end
+        t0d0_finder do
+          command 'blubb-blubb'
+          reversed_exit true
+        end
+      end
+    end
+    expect(BinCheck.new_script_hash)
+      .to eq(rubocop: { files: 'lib/**/*.rb', command: 'rubocop', reversed_exit: false },
+             t0d0_finder: { files: 'lib/**/*.rb', command: 'blubb-blubb', reversed_exit: true })
+  end
+
   it 'calls super if no block is given' do
     expect { BinCheck.blubb }.to raise_error(NoMethodError)
   end
